f14dd59399dd592d504481f3d2eebc83bcec6239
nijialoop <nijia@126.com>
2019-06-14 01:14:45 +0800

    Add upgrade script

---
+++ upfwd.sh
@@ -0,0 +1,6 @@
+sudo systemctl stop mmdvmhost.timer
+sudo systemctl stop mmdvmhost.service
+sudo stm32flash -b 115200 -v -w mmdvm1416C0418_DOWN.bin -g 0x0 -R -i 20,-21,21:-20,-21,21 /dev/ttyAMA0
+sudo systemctl start mmdvmhost.service
+sudo systemctl start mmdvmhost.timer
+
