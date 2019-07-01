069e43ec18ab6a92d345772a616e63c467d1ad2a
nijialoop <nijia@126.com>
2019-07-02 02:06:20 +0800

    Remove files

--- upfwusb.sh
+++
@@ -1,6 +0,0 @@
-curl -OL https://github.com/nijialoop/toybricktest/raw/master/1416C_USB.bin
-sudo systemctl stop mmdvmhost.timer
-sudo systemctl stop mmdvmhost.service
-sudo stm32flash -b 115200 -v -w 1416C_USB.bin -g 0x0 -R -i 20,-21,21:-20,-21,21 /dev/ttyAMA0
-sudo systemctl start mmdvmhost.service
-sudo systemctl start mmdvmhost.timer
