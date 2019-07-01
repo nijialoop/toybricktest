58aadda1c9cf7dd29fc748a715d2d0f4f319b076
nijialoop <nijia@126.com>
2019-07-02 01:44:53 +0800

    Add $1 to make filename variable.

--- upfw.sh
+++ upfw.sh
@@ -1,3 +1,4 @@
+curl -OL https://github.com/nijialoop/toybricktest/raw/master/$1
 sudo systemctl stop mmdvmhost.timer
 sudo systemctl stop mmdvmhost.service
 sudo stm32flash -b 115200 -v -w $1 -g 0x0 -R -i 20,-21,21:-20,-21,21 /dev/ttyAMA0
